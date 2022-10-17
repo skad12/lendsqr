13226e28001d687f3d44ac819c544a14569f717e
skad12 <dammykunle79@gmail.com>
2022-10-17 11:47:44 +0100

    lendsqr

---
+++ pages/text.js
@@ -0,0 +1,168 @@
+function test() {
+    return ( 
+
+        <div className='mt-20  h-98 w-full rounded-md text-gray-800 bg-white shadow-lg grid grid-cols-7'>
+  <div className='flex space-x-12 pt-5 pl-10 pb-2 '>
+
+  <div className=' font-bold space-x-1 text-xs'>
+<div className="flex ">
+    <div>ORGANIZATION</div>
+    <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+</svg></div>
+</div>
+
+<div className="space-y-5">
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+</div>
+    </div>
+
+    <div className=' font-bold space-x-1 text-xs'>
+<div className="flex ">
+    <div>ORGANIZATION</div>
+    <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+</svg></div>
+</div>
+
+<div className="space-y-5">
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+</div>
+    </div>
+
+    <div className=' font-bold space-x-1 text-xs'>
+<div className="flex ">
+    <div>ORGANIZATION</div>
+    <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+</svg></div>
+</div>
+
+<div className="space-y-5">
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+</div>
+    </div>
+
+    <div className=' font-bold space-x-1 text-xs'>
+<div className="flex ">
+    <div>ORGANIZATION</div>
+    <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+</svg></div>
+</div>
+
+<div className="space-y-5">
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+</div>
+    </div>
+
+    <div className=' font-bold space-x-1 text-xs'>
+<div className="flex ">
+    <div>ORGANIZATION</div>
+    <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+</svg></div>
+</div>
+
+<div className="space-y-5">
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+</div>
+    </div>
+    
+    
+
+
+    <div className=' font-bold space-x-1 text-xs'>
+
+<div className="flex ">
+    <div>ORGANIZATION</div>
+    <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+</svg></div>
+</div>
+
+<div className="space-y-5">
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+</div>
+    </div>
+  
+    
+
+    <div className=' font-bold space-x-1 text-xs'>
+<div className="flex ">
+    <div>ORGANIZATION</div>
+    <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+</svg></div>
+</div>
+
+<div className="space-y-5">
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+<div className=''>lendsqr</div>
+</div>
+    </div>
+   </div>
+   
+
+
+  </div>
+
+     );
+}
+
+export default test;
