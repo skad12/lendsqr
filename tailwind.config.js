86ea2f1a789b4256f338ad3ba475ac28e54ed0ed
skad12 <dammykunle79@gmail.com>
2023-01-03 02:25:59 +0100

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-
-
 module.exports = {
-  content: [ 
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'
-   
-
   ],
   theme: {
     extend: {
       fontFamily : {
         'sans' : 'Work Sans',
       }
-      
     },
-
   },
-  plugins: [
-    require('tw-elements/dist/plugin')
-  ],
+  plugins: [],
 }
